f794a95e4129578900bc67f5e212200304c09d1f
Dane Dunlap <danedunlap@Danes-MacBook-Pro.local>
2023-08-28 21:31:27 +0400

    Made necessary changes for database setup.

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,21 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
+import os
 
 
 
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lumis.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
+
+
+
+
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
