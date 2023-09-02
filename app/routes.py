504abe04f7bee448f2b373f70ac8df54b66d3480
Dane Dunlap <danedunlap@Danes-MacBook-Pro.local>
2023-09-02 14:33:07 +0400

    here is a commit

--- app/routes.py
+++ app/routes.py
@@ -106,14 +106,17 @@ def send_lumis():
     alert = db.session.query(Alert).filter_by(id=alert_id).first()
 
     articles = fetch_articles_for_alert(alert)
-    final_summary = articles_summarizer(articles)
-    #final_summary = "Apple has released two new features for iOS users – Remove Subject From Background and Create and Save Your Own Stickers – to enhance their photo-editing experience. Remove Subject offers the ability to quickly and easily erase unwanted people and objects from photos, while Create and Save Your Own Stickers lets users turn their own snapshots and text into custom stickers. Additionally, the AirPods Pro 2 has been upgraded to include improved sound quality, active noise cancellation, spatial audio, transparency mode, and"
+    #final_summary = articles_summarizer(articles)
+    final_summary = "Apple has released two new features for iOS users – Remove Subject From Background and Create and Save Your Own Stickers – to enhance their photo-editing experience. Remove Subject offers the ability to quickly and easily erase unwanted people and objects from photos, while Create and Save Your Own Stickers lets users turn their own snapshots and text into custom stickers. Additionally, the AirPods Pro 2 has been upgraded to include improved sound quality, active noise cancellation, spatial audio, transparency mode, and"
     subject = f"Lumis Alert: {alert.company_name}"
     recipient_email = alert.user_email
     url_list = [article['url'] for article in articles]    
     send_email(subject,final_summary,recipient_email,url_list,alert)
     
-    return jsonify({"message": "Success."}), 200
+    try:
+        return jsonify({"message": "Success"}), 201
+    except:
+        return jsonify({"message": "There was an error sending this alert to email"}), 500
 
 
 def fetch_articles_for_alert(alert):
