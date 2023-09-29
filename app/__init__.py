de8bd1a8256fe9151a9dbc927b70d788753b436f
Dane Dunlap <danecooperdunlap@gmail.com>
2023-09-29 22:45:01 +0400

    mking a commit

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ import os
 
 
 app = Flask(__name__)
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://")
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app, resources={r"/api/*": {"origins": "https://lumis-react.vercel.app"}})
